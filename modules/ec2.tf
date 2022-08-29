6f0dc6a63cf3bda5f3cf87cdf568c5ac5134e016
Adeniji Opeyemi <adenijiopeyemi68@gmail.com>
2022-08-29 17:35:08 +0100

    made changes

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,15 +1,15 @@
 # launch instance in each public subnet
 resource "aws_instance" "app-db" {
-    ami = var.ami
-    instance_type = var.instance_type
+  ami           = var.ami
+  instance_type = var.instance_type
 
-    subnet_id = "${aws_subnet.public-subnet.id}"
-    vpc_security_group_ids = ["${aws_security_group.wp-sg.id}"]
-    key_name = var.key_name
+  subnet_id              = aws_subnet.public-subnet.id
+  vpc_security_group_ids = ["${aws_security_group.wp-sg.id}"]
+  key_name               = var.key_name
 
-    tags = {
-        Name: "APP-Instance"
-    }
+  tags = {
+    Name : "APP-Instance"
+  }
 
-    #   user_data = "${file("user_data.sh")}"
+  #   user_data = "${file("user_data.sh")}"
 }
--- modules/variable.tf
+++ modules/variable.tf
@@ -4,27 +4,27 @@ variable "region" {
 }
 
 variable "key_name" {
-        description   = "This is key used to authenticate or ssh into the ec2 instance"
+  description = "This is key used to authenticate or ssh into the ec2 instance"
 
-   type = string
+  type = string
 }
 
 variable "availability_zone" {
-      description   = "This is the availability zone for the resources created"
-   type = string
+  description = "This is the availability zone for the resources created"
+  type        = string
 }
 
 variable "availability_zone2" {
-    description   = "This is the availability zone for the resources created"
-    type          = string
+  description = "This is the availability zone for the resources created"
+  type        = string
 }
 
 variable "instance_type" {
   description = "This is the instance type for our instance" # it could be "t2.micro", "t3.micro", etc
-  type = string
+  type        = string
 }
 
 variable "ami" {
   description = "This is the amazon machine imagefor our instance"
-  type = string
+  type        = string
 }
